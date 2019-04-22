50fcc5da2ac90c985443abfd1daaa96232369064
lydmgem <lydmgem@gmail.com>
2019-04-22 18:22:27 -0500

    got github pages to work just now... so far

---
+++ build/precache-manifest.d06d8d8db89528928dac2b239effa127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d129b6e6e185bea2d18",
+    "url": "/REACT-Clicky-Game/static/css/main.8c978495.chunk.css"
+  },
+  {
+    "revision": "6d129b6e6e185bea2d18",
+    "url": "/REACT-Clicky-Game/static/js/main.882fc97c.chunk.js"
+  },
+  {
+    "revision": "3d516bbda9a9ffc7379d",
+    "url": "/REACT-Clicky-Game/static/js/runtime~main.f830af74.js"
+  },
+  {
+    "revision": "4743c039c04edea7e174",
+    "url": "/REACT-Clicky-Game/static/js/2.1db7aab6.chunk.js"
+  },
+  {
+    "revision": "1baf6ab96fa9d321deb061ac227662ba",
+    "url": "/REACT-Clicky-Game/static/media/background.1baf6ab9.jpg"
+  },
+  {
+    "revision": "a7c43db828476e58c1e85a27e4b37fe6",
+    "url": "/REACT-Clicky-Game/index.html"
+  }
+];
